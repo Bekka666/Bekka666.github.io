6ced940185799767a1285f2b8a6fbec8e78f91b5
Bekka666 <wrig0310@algonquinlive.com>
2014-11-06 09:37:52 -0500

    Added parallax to header section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 // JavaScript Document
 
 // Toggle Nav JS
+var $win = $(window);
 var navTop = document.querySelector('.nav-top');
+var $logo = $('.logo');
+var $headerSection = $('.header-section');
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+
+	$logo.css('transform', 'rotate(' + scrollPos / 1 + 'deg)');
+	$headerSection.css('background-position', 'center ' + scrollPos / 1 + 'px');
+});
+
+$headerSection.waypoint(function () {
+	$logo.addClass('js-logo-fade');
+	
+}, { offset: '50%' });
 	
 document.querySelector('.nav-btn').addEventListener('click', function (e) { 
 		e.preventDefault();
