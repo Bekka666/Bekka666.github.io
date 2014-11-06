398ca673e6ec842a4e8e7be698f28ae5fda4e803
Bekka666 <wrig0310@algonquinlive.com>
2014-11-06 13:54:36 -0500

    Styled colours

--- js/main.js
+++ js/main.js
@@ -6,17 +6,26 @@ var navTop = document.querySelector('.nav-top');
 var $logo = $('.logo');
 var $headerSection = $('.header-section');
 var $portfolioSection = $('#portfolio');
+var contact = $('.contact');
+var contactDetails = $('.contact-details');
+var $photo = $('.photo');
+var $about = $('.about');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 
 	$logo.css('transform', 'rotate(' + scrollPos / 1 + 'deg)');
 	$headerSection.css('background-position', 'center ' + scrollPos / 1 + 'px');
-	$portfolioSection.css('margin-top', '-' + scrollPos / 5 + 'px');
+	$portfolioSection.css('margin-top', '-' + scrollPos / .8 + 'px');
 });
 
 $headerSection.waypoint(function () {
 	$logo.addClass('js-logo-fade');
+	
+}, { offset: '50%' });
+
+$about.waypoint(function () {
+	$photo.addClass('js-photo-slide');
 	
 }, { offset: '50%' });
 	
@@ -32,3 +41,13 @@ if (navTop.getAttribute('data-state') == 'expanded'){
 }
 	
 });
+
+var buttonClickHandler = function () {
+var currentState = contactDetails.attr('data-state');
+
+if (currentState == 'active') {
+	contactDetails.attr('data-state', 'inactive');
+} else {
+	contactDetails.attr('data-state', 'active');
+}
+};
